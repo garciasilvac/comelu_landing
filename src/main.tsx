7e9b9d05069f3384dff03c7ad25838698c316e3f
Carlos García <garciasilvac@uc.cl>
2026-03-03 00:38:05 -0300

    feat: initialize scroll reveal on app startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { initScrollReveal } from "./lib/scrollReveal";
 import "./index.css";
+
+function RootApp() {
+  React.useEffect(() => {
+    const cleanup = initScrollReveal();
+    return cleanup;
+  }, []);
+
+  return <App />;
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RootApp />
   </React.StrictMode>,
 );
